2d76de5bd39aea421d1d1352fa1217f0a2db45a9
Convidat <convidat@itequia.local>
2018-05-15 13:00:25 +0200

    Example MVC

---
+++ WebApplication1/WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index(string name, int numTimes)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }
+    }
+}
